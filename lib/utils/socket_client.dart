178cefb6b22bb244c1bfbd2d02daa4ff20336484
arshanishad <arshanishad5@gmail.com>
2026-05-27 17:40:42 +0530

    socket connection code completed

--- lib/utils/socket_client.dart
+++ lib/utils/socket_client.dart
@@ -1,14 +1,24 @@
-
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class SocketClient {
   IO.Socket? socket;
-  SocketClient._internal(){
-     socket=IO.io('http://192.168.31.21:3000',<String,dynamic>{
-      'transports':['websocket'],
-      'autoConnect':false,
-     }) ;
-     socket!.connect();
+
+  // Private static variable
+  static SocketClient? _instance;
+
+  // Private constructor
+  SocketClient._internal() {
+    socket = IO.io('http://192.168.31.21:3000', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+
+    socket!.connect();
   }
 
+  // Public getter to access instance
+  static SocketClient get instance {
+    _instance ??= SocketClient._internal();
+    return _instance!;
+  }
 }
