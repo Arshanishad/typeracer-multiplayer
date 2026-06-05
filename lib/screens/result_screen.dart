13b62ed8b2ead3f137fe54d276619d6d574d5b29
arshanishad <arshanishad5@gmail.com>
2026-06-05 10:31:39 +0530

    result screen

---
+++ lib/screens/result_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  final String winner;
+  final double wpm;
+  final double accuracy;
+
+  const ResultScreen({
+    super.key,
+    required this.winner,
+    required this.wpm,
+    required this.accuracy,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Results")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("🏆 Winner: $winner",
+                style: const TextStyle(fontSize: 22)),
+            const SizedBox(height: 20),
+
+            Text("Speed: ${wpm.toStringAsFixed(1)} WPM"),
+            Text("Accuracy: ${accuracy.toStringAsFixed(1)}%"),
+            
+            const SizedBox(height: 30),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.popUntil(context, (route) => route.isFirst);
+              },
+              child: const Text("Back to Home"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
