55d667e488e6f23db76cb86097edd1f6bad4f7a9
arshanishad <arshanishad5@gmail.com>
2026-06-05 12:55:50 +0530

    index.js file

--- server/index.js
+++ server/index.js
@@ -1,52 +1,50 @@
-// 1. Import required packages
+//1. Import required packages
 const express = require('express');
 const http = require('http');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const { Server } = require('socket.io');
-const Game = require ('./models/Game');
+const Game = require('./models/Game');
 
-// 2.Create a new backend application (server app) using Express
+//2. Create express app
 const app = express();
 
-// 3. Define port
+//3. Port
 const port = process.env.PORT || 3000;
 
-// 4. Create HTTP server using Express app
+//4. Create HTTP server
 const server = http.createServer(app);
 
-// 5. Attach Socket.IO to server
+//5. Attach Socket.IO
 const io = new Server(server);
 
-// 6. Middleware (allow server to read JSON data)
+//6. Middleware
 app.use(express.json());
 
-// 7. Connect to MongoDB
+//7. MongoDB connection
 const DB = process.env.MONGO_URI;
-
 mongoose.connect(DB)
-    .then(() => {
-        console.log('Database Connected');
-    })
-    .catch((e) => {
-        console.log(e);
-    });
+    .then(() => console.log('Database Connected'))
+    .catch((e) => console.log(e));
 
-// 8. Socket connection -listening to socket io events from the client (flutter code )
+//8. Socket connection
 io.on('connection', (socket) => {
-    console.log('User connected:', socket.id);
+    console.log('User Connected:', socket.id);
 
-    // 9. Listen event from Flutter
+    //9. Listen event from Flutter
     socket.on('create-game', async ({ nickname }) => {
         try {
-            let game = new Game();
+            let game = new Game({
+                nickname
+            });
+            await game.save();
+            //10. Send response
+            socket.emit('game-created', {
+                message: 'Game Created Successfully'
+            });
         } catch (e) {
             console.log(e);
         }
-        // 10. Send response back
-        socket.emit('game-created', {
-            message: 'Game created successfully'
-        });
     });
 
     // 11. Disconnect
@@ -55,7 +53,7 @@ io.on('connection', (socket) => {
     });
 });
 
-// 12.Start the server on a given port and make it listen for incoming requests
+// 12. Start server
 server.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port ${port}`);
 });
