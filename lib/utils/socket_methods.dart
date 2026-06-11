e4b4c83f6f195f5f68e9bcb75c7be732b0bb4419
arshanishad <arshanishad5@gmail.com>
2026-06-11 12:12:21 +0530

    socket methods

--- lib/utils/socket_methods.dart
+++ lib/utils/socket_methods.dart
@@ -4,6 +4,7 @@ class SocketMethods{
 final _socketClient=SocketClient.instance.socket;
 
 //create game
+// Hey server ,create a game 
 void createGame(String nickname){
   if(nickname.isNotEmpty){
     _socketClient?.emit('create-game',{
